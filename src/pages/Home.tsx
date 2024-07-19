69bfe67d89e4d15ebcf06692582353e333256235
shakedown3000 <annalaube90@gmail.com>
2024-07-19 14:27:05 +0200

    done with first function edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,44 @@ import { useState } from "react";
 
 const Home = () => {
   const rockpaperscissorsArray: string[] = ["Rock", "Paper", "Scissors"];
-  const [myResult, setMyResult] = useState<string>();
+  const [yourResult, setYourResult] = useState<string>();
   const [cpuResult, setCpuResult] = useState<string>();
   const [roundsWinner, setRoundsWinner] = useState<string>();
+  const [yourScore, setYourScore] = useState<number>(0);
+  const [cpuScore, setCpuScore] = useState<number>(0);
+  const [numberOfRounds, setNumberOfRounds] = useState<number>(1);
+  const [allRoundsWinner, setAllRoundsWinner] = useState<string>("");
 
   function cpuPlays(array: string[]): string {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
   }
 
+  function checkWinner() {
+    console.log("Check Wimmer Function activated");
+    if (yourScore > cpuScore) {
+      setAllRoundsWinner("You!");
+    } else if (yourScore < cpuScore) {
+      setAllRoundsWinner("The computer");
+    } else {
+      setAllRoundsWinner("It is a tie!");
+    }
+  }
+
+  function restartGame() {
+    setCpuResult("");
+    setCpuScore(0);
+    setYourResult("");
+    setYourScore(0);
+    setNumberOfRounds(1);
+    setRoundsWinner("");
+  }
+
   const playGame = (yourChoice: string) => {
+    setAllRoundsWinner("");
     console.log(`You chose: ${yourChoice}`);
     const computersChoice: string = cpuPlays(rockpaperscissorsArray);
-    setMyResult(yourChoice);
+    setYourResult(yourChoice);
     setCpuResult(computersChoice);
     console.log(`The computer chose: ${computersChoice}`);
     const combinedChoices = `${yourChoice}-${computersChoice}`;
@@ -26,6 +51,7 @@ const Home = () => {
       case "Paper-Rock":
         console.log("You won.");
         setRoundsWinner(" You won this round! 🥳");
+        setYourScore(yourScore + 1);
         break;
       // CPU wins (last Value wins)
       case "Scissors-Rock":
@@ -33,10 +59,18 @@ const Home = () => {
       case "Rock-Paper":
         console.log("CPU won");
         setRoundsWinner(" The computer won this round! 💻");
+        setCpuScore(cpuScore + 1);
         break;
       default:
         console.log("It's a tie!");
         setRoundsWinner(" It's a tie! 😮");
+    }
+
+    setNumberOfRounds(numberOfRounds + 1);
+
+    if (numberOfRounds === 3) {
+      checkWinner();
+      restartGame();
     }
   };
 
@@ -46,7 +80,7 @@ const Home = () => {
         <h1>Rock, Paper, Scissors</h1>
         <h2>Choose your weapon:</h2>
         <p id="you_chose" className="answer">
-          You chose: <span> {myResult}</span>
+          You chose: <span> {yourResult}</span>
         </p>
         <p id="cpu_chose" className="answer">
           The CPU chose: <span> {cpuResult}</span>
@@ -74,17 +108,24 @@ const Home = () => {
               <tr>
                 <th>Your Score</th>
                 <th>CPU Score</th>
+                <th>Number of Rounds</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>0</td>
-                <td>0</td>
+                <td>{yourScore}</td>
+                <td>{cpuScore}</td>
+                <td>{numberOfRounds}/3</td>
               </tr>
             </tbody>
           </table>
+          <p>
+            The overall winner is: <span>{allRoundsWinner}</span>
+          </p>
         </div>
-        <div className="restart">Restart Game</div>
+        <div className="restart" onClick={restartGame}>
+          Restart Game
+        </div>
       </section>
     </>
   );
